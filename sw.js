f8d9a7fec3d3e96de35da6fd9ed43458946187e3
Chimpan <o.mitsumoto@gmail.com>
2026-03-24 01:16:52 +0900

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,34 @@
+// Service Worker for Sapiens Poker Logger v0.2 PWA
+const CACHE_NAME = 'sapiens-poker-logger-v02-v1';
+const ASSETS = [
+  './',
+  './index.html',
+  './icon-192.png',
+  './icon-512.png',
+  './icon-192-maskable.png',
+  './icon-512-maskable.png',
+  './manifest.json',
+  './title_image_lv07.png'
+];
+
+self.addEventListener('install', e => {
+  e.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS))
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', e => {
+  e.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k)))
+    ).then(() => clients.claim())
+  );
+});
+
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    caches.match(e.request).then(r => r || fetch(e.request))
+      .catch(() => caches.match('./index.html'))
+  );
+});
